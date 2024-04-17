49182e33739a62624c22b62dc16e5f81edb1de72
Jennifer <jmartinez28728@ucq.edu.mx>
2024-04-17 17:28:04 -0600

    Proyecto final

--- react-ordering-system/src/views/Login.jsx
+++ react-ordering-system/src/views/Login.jsx
@@ -68,7 +68,7 @@ export default function Login() {
               <input
                   type="submit"
                   value="Log in"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"                    
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"                    
                   href="#"
               />
 
--- react-ordering-system/src/views/Register.jsx
+++ react-ordering-system/src/views/Register.jsx
@@ -97,7 +97,7 @@ export default function Register() {
                 <input
                     type="submit"
                     value="Sign up"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"                    
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"                    
                     href="#"
                 />
 
